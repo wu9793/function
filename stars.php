f0b822973ee5d6d7baa466d7df346be35b5be36e
wu <timmy61501@gmail.com>
2023-11-17 13:40:33 +0800

    function_test_菱形方形

--- stars.php
+++ stars.php
@@ -1,5 +1,5 @@
 <style>
-    *{
+    * {
         font-family: 'Courier New', Courier, monospace;
     }
 </style>
@@ -11,16 +11,64 @@ triangle(20);
 
 
 
-function triangle($size){
-    for($i=0;$i<$size;$i++){ //行列數
-    for($j=0;$j<($size-1-$i);$j++){ //內圈印空格數
-        echo "&nbsp;";
-    }    
-    for($k=0;$k<($i*2+1);$k++){ //內圈印星星
-          echo "*";  
+function triangle($size)
+{
+    for ($i = 0; $i < $size; $i++) { //行列數
+        for ($j = 0; $j < ($size - 1 - $i); $j++) { //內圈印空格數
+            echo "&nbsp;";
+        }
+        for ($k = 0; $k < ($i * 2 + 1); $k++) { //內圈印星星
+            echo "*";
+        }
+
+        echo "<br>";
     }
-     
-echo "<br>";
+}
+
+
+diamond(7);
+
+function diamond($size){
+    $mid = floor(($size * 2 - 1) / 2);
+    $temp = 0;
+    for ($i = 0; $i < ($size * 2 - 1); $i++) {
+
+        if ($i <= $mid) {
+            $temp = $i;
+        } else {
+            $temp--;
+        }
+
+        for ($j = 0; $j < ($mid - $temp); $j++) {
+            echo "&nbsp;";
+        }
+        for ($k = 0; $k < ($temp * 2 + 1); $k++) {
+            echo "*";
+        }
+
+        echo "<br>";
+    }
+}
+
+reangle(5);
+reangle(7);
+reangle(9);
+
+
+function reangle($size){
+    for($i=0;$i<$size;$i++){
+
+    for($j=0;$j<$size;$j++){
+        if($i==0 || $i==($size-1)){
+        echo "*";
+        }else if($j==0 || $j==($size-1)){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    
+    }
+    echo "<br>";
 }
 }
 
