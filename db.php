c35db3eaca7770255fb02c85a60fba8f69404b00
wu <timmy61501@gmail.com>
2023-11-17 16:29:35 +0800

    增加第三個變數

--- db.php
+++ db.php
@@ -1,26 +1,40 @@
 <?php
 
-$rows = all('students');
+$rows = all('students',['dept'=>'3']);
 
 dd($rows);
 
-function all($table=null)
-{
+function all($table=null,$where='',$other=''){
+
     $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     $pdo = new PDO($dsn, 'root', '');
+    $sql = "select * from `$table` ";
 
     if (isset($table) && !empty($table)) {
-        $sql = "select * from `$table` ";
+
+        if(is_array($where)){
+            
+        
+        if(!empty($where)){
+                foreach($where as $col => $value){
+                    $tmp[]="`$col`='$value'";
+                }
+                $sql .= " where ".join(" && ",$tmp);
+            }
+        }else{
+            $sql .= " $where";
+        }
+        $sql .=$other;
         $rows = $pdo->query($sql)->fetchAll();
         return $rows;
     }else{
         echo "錯誤:沒有指定的資料表名稱";
     }
 }
-
-function dd($array)
-{
+    function dd($array){
     echo "<pre>";
     print_r($array);
     echo "</pre>";
 }
+
+?>
